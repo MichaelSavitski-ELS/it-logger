f37c656ab34fc9c5fb588a89c9732b37003fae09
MichaelSavitski-ELS <m.savitski@elsevier.com>
2021-02-17 16:11:16 -0500

    Lessons 77-78

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
@@ -9,9 +11,12 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
